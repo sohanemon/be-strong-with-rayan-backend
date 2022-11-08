b40cc21c82c83923cbfd5c6e4b00b51df72e1656
sohanemon <m.sohanemon@gmail.com>
2022-11-08 23:46:15 +0600

    services post added

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+app.use(cors());
+app.use(express.json());
 const port = 5000;
+const client = new MongoClient(process.env.URI);
+const serviceCollection = client.db("be-strong").collection("services");
+try {
+  app.get("/", (req, res) => {
+    res.send("Be Strong with Rayan is running...");
+  });
+  app.post("/services", async (req, res) => {
+    const result = await serviceCollection.insertOne(req.body);
+    res.send(result);
+  });
+} catch (error) {
+  console.log("🚀 > error", error);
+}
 
-app.get("/", (req, res) => {
-  res.send("Be Strong with Rayan is running...");
-});
 app.listen(port, () => console.log(`App listening on port ${port}!`));
